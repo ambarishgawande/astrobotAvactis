c29822294a0bdfa61500b64e683883d241eed214
ambarishgawande <ambarish.lg@gmail.com>
2019-10-07 16:04:59 +0530

    Home page test case is implemented

---
+++ src/test/java/com/avactis/alg/qa/testcases/HomePageTest.java
@@ -0,0 +1,57 @@
+package com.avactis.alg.qa.testcases;
+
+import org.testng.annotations.Test;
+
+import com.avactis.alg.qa.base.ProjectBase;
+import com.avactis.alg.qa.pages.ApparelPage;
+import com.avactis.alg.qa.pages.HomePage;
+import com.avactis.alg.qa.pages.LoginPage;
+
+import org.testng.annotations.BeforeClass;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class HomePageTest extends ProjectBase {
+	
+	LoginPage loginPage;
+	HomePage homePage;
+	ApparelPage apparelPage;
+	
+	public HomePageTest() {
+		super();
+	}
+	
+  @Test(priority=1)
+  public void verifyHomePageTitleTest() {
+	  
+	  Assert.assertEquals(homePage.verifyHomePageTitle(), "Avactis Demo Store", "Home page title not match");
+  }
+  
+  @Test(priority=2)
+  public void verifyWelcomeTextTest() {
+	  Assert.assertTrue(homePage.verifyWelcomeText(), "Welcome text not match");
+  }
+  
+  @Test(priority=3)
+  public void verifyUserNameTest() {
+	  Assert.assertTrue(homePage.verifyUserName(), "User Name not match");
+  }
+  
+  
+  
+  
+  @BeforeClass
+  public void beforeClass() {
+	  initializationOfBrowser("GoToUserPage");
+	  loginPage = new LoginPage();
+	  homePage = loginPage.login(prop.getProperty("userName"), prop.getProperty("userPassword"));
+	  
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
