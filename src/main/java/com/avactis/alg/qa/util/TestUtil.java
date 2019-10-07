e1a11329492e425800577ad5cc51006890181846
ambarishgawande <ambarish.lg@gmail.com>
2019-10-07 16:07:10 +0530

    switch to frame function implemented in TestUtil.java

--- src/main/java/com/avactis/alg/qa/util/TestUtil.java
+++ src/main/java/com/avactis/alg/qa/util/TestUtil.java
@@ -1,9 +1,14 @@
 package com.avactis.alg.qa.util;
 
-public class TestUtil {
+import com.avactis.alg.qa.base.ProjectBase;
+
+public class TestUtil extends ProjectBase {
 	
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 20;
 	
+	public void switchToFrame(String frameName) {
+		driver.switchTo().frame(frameName);
+	}
 
 }
