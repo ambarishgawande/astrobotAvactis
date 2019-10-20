1bffc19c3f0e9aba6705e022023c213df31b7197
ambarishgawande <ambarish.lg@gmail.com>
2019-10-20 17:57:44 +0530

    Purchase test case implementation

---
+++ src/test/java/com/avactis/alg/qa/testcases/PurchaseTest.java
@@ -0,0 +1,21 @@
+package com.avactis.alg.qa.testcases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class PurchaseTest {
+
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void f() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
